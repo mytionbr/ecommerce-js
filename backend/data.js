af107ef92cf14567942c76cad84aa72f6ad9ba66
mytionBr <matheuss.jesus2017@gmail.com>
2021-02-05 21:21:23 -0300

    atualizando o projeto

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+module.exports =  {
+    products: [
+        {
+            _id:'1',
+            name:'Immortals Fenyx Rising',
+            category:'Action',
+            image:'/images/capa1.jpg',
+            price:60,
+            brand:'Ubisoft',
+            rating:4.5,
+            numReviews:10,
+            countInStock:6,
+        },
+        {
+            _id:'2',
+            name:'Immortals Fenyx Rising',
+            category:'Action',
+            image:'/images/capa1.jpg',
+            price:60,
+            brand:'Ubisoft',
+            rating:4.5,
+            numReviews:10,
+            countInStock:6,
+        },
+        {
+            _id:'3',
+            name:'Immortals Fenyx Rising',
+            category:'Action',
+            image:'/images/capa1.jpg',
+            price:60,
+            brand:'Ubisoft',
+            rating:4.5,
+            numReviews:10,
+            countInStock:6,
+        },
+
+    ]
+}
