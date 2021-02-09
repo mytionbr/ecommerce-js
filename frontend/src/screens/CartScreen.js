b27225e3ff0260bcc57524ceb42b9886d89e07d1
mytionBr <matheuss.jesus2017@gmail.com>
2021-02-09 16:28:13 -0300

    implementando ações do cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,22 +1,53 @@
 import { getProduct } from "../api"
 import { getCartItems, setCartItems } from "../localStorage"
-import { parseRequestUrl } from "../utils"
+import { parseRequestUrl, rerender } from "../utils"
 
 const addToCart = (item,forceUpdate = false) =>{
     let  cartItems = getCartItems()
     const existItem = cartItems.find(x=>x.product === item.product)    
     if(existItem){
-        cartItems = cartItems.map((x)=>x.product === existItem.product?item:x)
+        if(forceUpdate){
+            cartItems = cartItems.map((x)=>x.product === existItem.product?item:x)
+        }
+        
     }else{
         cartItems = [...cartItems, item]
     }
     setCartItems(cartItems)
+    if(forceUpdate){
+        rerender(CartScreen)
+    }
+   
+}
+
+const removeFromCart = (id) =>{
+    setCartItems(getCartItems().filter((x)=>x.product !== id))
+    if(id === parseRequestUrl().id){
+        document.location.hash = '/cart'
+    }else {
+        rerender(CartScreen)
+    }
 }
 
 const CartScreen = {
     
     after_render: ()=>{
-
+        const qtySelects = document.getElementsByClassName('qty-select')
+        Array.from(qtySelects).forEach((qtySelect)=>{
+            qtySelect.addEventListener('change',(e)=>{
+                const item = getCartItems().find((x)=> x.product === qtySelect.id)
+                addToCart({...item,qty:Number(e.target.value)},true)
+            })
+        })
+        const deleteButtons = document.getElementsByClassName("delete-button")
+        Array.from(deleteButtons).forEach((deleteButton) =>{
+            deleteButton.addEventListener('click',()=>{
+                removeFromCart(deleteButton.id)
+            })
+        })
+        document.getElementById("chechout-button").addEventListener("click",()=>{
+            document.location.hash = '/singin'
+        })
     },
     render: async () => {
         const request = parseRequestUrl()
@@ -55,9 +86,16 @@ const CartScreen = {
                                         </a>
                                     </div>
                                     <div>
-                                        Qty: <select class="qty-select" id="${item.product}">
-                                        <option value='1'>1</option>
-                                        <select>
+                                        Qty:
+                                       <select class="qty-select" id="${item.product}">
+                                       ${
+                                        [...Array(item.countInStock).keys()].map((x)=>
+                                        item.qty === x+1
+                                         ? `<option selected value="${x+1}"> ${x+1}</option>`
+                                         : `<option value="${x+1}"> ${x+1}</option>`
+                                         )}
+                                        </select>
+
                                         <button type="button" class="delete-button" id="${item.product}">
                                             Delete
                                         </button>
@@ -77,7 +115,7 @@ const CartScreen = {
                         :
                        $${cartItems.reduce((a,c)=>a+c.price * c.qty,0)}
                     </h3>
-                    <button id="checkout-button" class="primary fw">
+                    <button id="chechout-button" class="primary fw">
                         Procced to Checkout
                     </button>
             </div>
